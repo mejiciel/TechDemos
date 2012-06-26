0302684d78a7a38f8146c92dcd955d7d40be7279
mejiciel <mejiciel@gmail.com>
2012-06-26 18:01:12 +0800

    demo update

--- UserDatabaseDemo/src/com/meji/UserManagement/Test/UserDBTest.java
+++ UserDatabaseDemo/src/com/meji/UserManagement/Test/UserDBTest.java
@@ -2,7 +2,11 @@ package com.meji.UserManagement.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.hibernate.Query;
@@ -19,6 +23,7 @@ public class UserDBTest {
 
 	private SessionFactory mysqlFactory;
 	
+	
 	@Before
 	public void setUp() throws Exception {
 		
@@ -26,36 +31,87 @@ public class UserDBTest {
 		cfg.configure();
 		ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(cfg.getProperties()).buildServiceRegistry(); 
 		mysqlFactory = cfg.buildSessionFactory(serviceRegistry);
+		
+		Session s=mysqlFactory.getCurrentSession();
+		Transaction t=s.beginTransaction();
+		
+		//List usps=s.createSQLQuery("select * from userprofile").list();
+		List<UserProfile> usps=GenerateUserProfile();
+		for(UserProfile usp : usps)
+		{
+			
+			//System.out.println(usp);
+			//s.delete(usp);
+			s.createSQLQuery("DELETE FROM sitemanagement.userprofile WHERE UserName=?").setString(0, usp.getName()).executeUpdate();
+		}
+		t.commit();
+		
 
 	}
 
 	@Test
-	public void test() {
+	public void InsertTest() {
 		Session s=mysqlFactory.getCurrentSession();
 		Transaction t=s.beginTransaction();
+		List<UserProfile> usps=GenerateUserProfile();
+		for(UserProfile usp : usps)
+		{
+			
+			Integer id=(Integer)s.save(usp);
+			Assert.assertTrue("id<0", id>0);
+		}
+		t.commit();
+
+
+	}
+	@Test
+	public void QueryTest()
+	{
+		InsertTest();
+		Session s=mysqlFactory.getCurrentSession();
+		Transaction t=s.beginTransaction();
+		List<UserProfile> lst=s.createQuery("from UserProfile where UserName=:NAME").setString("NAME", "meji").list();
+		Assert.assertEquals("meji", lst.get(0).getName());
+		
+		//UserProfile usp=new UserProfile();
+		//usp.setName("meji");
+		//Assert.assertTrue("user not found",s.contains(usp));
+		
+	}
+	
+	@Test
+	public void UpdateTest()
+	{
+		InsertTest();
+		Session s=mysqlFactory.getCurrentSession();
+		Transaction t=s.beginTransaction();
+		List<UserProfile> lst=s.createQuery("from UserProfile where UserName=:NAME").setString("NAME", "meji").list();
+		lst.get(0).setName("mejimeji");
+		s.update(lst.get(0));
+		t.commit();
+		
+		s=mysqlFactory.getCurrentSession();
+		t=s.beginTransaction();
+		lst=s.createQuery("from UserProfile where UserName=:NAME").setString("NAME", "mejimeji").list();
+		Assert.assertTrue(lst.size()>0);
+	}
+	private List<UserProfile> GenerateUserProfile()
+	{
+		List<UserProfile> usps=new ArrayList<UserProfile>();
 		UserProfile usp=new UserProfile();
 		usp.setName("meji");
 		usp.setPassword("1234");
 		usp.setEmail("meji@123.com");
+		usps.add(usp);
 		
 		UserProfile usp2=new UserProfile();
 		usp2.setName("Fanfan");
 		usp2.setPassword("233444");
 		usp2.setEmail("fanfan@123.com");
-		s.save(usp);
-		s.save(usp2);
-		t.commit();
+		usps.add(usp2);
+		return usps;
 		
-		
-		/*s=mysqlFactory.getCurrentSession();
-		Query q=s.createQuery("select * from UserProfile where UserName='meji'");
-		q.e
-		t=s.beginTransaction();
-		s.delete(usp);
-		t.commit();*/
-
 	}
-	
 	@After
 	public void teadDown() throws Exception{
 		mysqlFactory.close();
---
+++ UserDatabaseDemo/src/com/meji/UserManagement/UserInfo.java
@@ -0,0 +1,46 @@
+package com.meji.UserManagement;
+
+public class UserInfo {
+	private int id;
+	private String address;
+	private String interests;
+	private String industry;
+	private String cellNumber;
+	private String homeNumber;
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getInterests() {
+		return interests;
+	}
+	public void setInterests(String interests) {
+		this.interests = interests;
+	}
+	public String getIndustry() {
+		return industry;
+	}
+	public void setIndustry(String industry) {
+		this.industry = industry;
+	}
+	public String getCellNumber() {
+		return cellNumber;
+	}
+	public void setCellNumber(String cellNumber) {
+		this.cellNumber = cellNumber;
+	}
+	public String getHomeNumber() {
+		return homeNumber;
+	}
+	public void setHomeNumber(String homeNumber) {
+		this.homeNumber = homeNumber;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+}
