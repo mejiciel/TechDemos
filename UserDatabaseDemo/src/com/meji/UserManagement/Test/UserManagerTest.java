523075ad921c484b607e6d1302a6f09b64aca6bf
mejiciel <mejiciel@gmail.com>
2012-06-28 18:12:13 +0800

    t

--- UserDatabaseDemo/src/com/meji/UserManagement/Test/UserManagerTest.java
+++ UserDatabaseDemo/src/com/meji/UserManagement/Test/UserManagerTest.java
@@ -5,6 +5,7 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 import com.meji.UserManagement.UserManager;
+import com.meji.UserManagement.UserProfile;
 
 public class UserManagerTest {
 
@@ -13,7 +14,28 @@ public class UserManagerTest {
 	{
 		UserManager umgr=new UserManager();
 		try {
-			Assert.assertTrue(umgr.CheckUserByName("meji"));
+			Assert.assertTrue(umgr.CheckUserExistByName("meji"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void CheckUserByObjectTest()
+	{
+		UserManager umgr=new UserManager();
+		UserProfile user=new UserProfile();
+		
+		
+		try {
+			user.setName("meji");
+			user.setName("meji@123.com");
+			Assert.assertTrue(umgr.CheckUserExist(user));
+			
+			user.setName("meji");
+			user.setName("mejimeji@123.com");
+			Assert.assertFalse(umgr.CheckUserExist(user));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- UserDatabaseDemo/src/com/meji/UserManagement/UserManager.java
+++ UserDatabaseDemo/src/com/meji/UserManagement/UserManager.java
@@ -19,6 +19,13 @@ public class UserManager {
 		mysqlFactory = cfg.buildSessionFactory(serviceRegistry);
 	}
 	
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
+		mysqlFactory.close();
+	}
+
 	public Boolean CheckUserExistByName(String userName) throws Exception
 	{
 		Session sess=mysqlFactory.getCurrentSession();
@@ -32,8 +39,18 @@ public class UserManager {
 		return true;
 		
 	}
-	public Boolean CheckUserExist(UserProfile user)
+	public Boolean CheckUserExist(UserProfile user) throws Exception
 	{
+		Session sess=mysqlFactory.getCurrentSession();
+		Transaction t=sess.beginTransaction();
+		List<UserProfile> usps=sess.createQuery("from UserProfile where UserName=:PARAM_NAME AND Email=:PARAM_EMAIL")
+				.setString("PARAM_NAME", user.getName())
+				.setString("PARAM_EMAIL", user.getEmail()).list();
+		if(usps.size()==0)
+			return false;
+		else if(usps.size()>1)
+			throw new Exception("Duplicated Name");
 		
+		return true;
 	}
 }
